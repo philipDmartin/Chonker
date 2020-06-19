4681935acd58f7fdffe79ff4a47a4c236b0d6301
philipDmartin <philipnathan788@gmail.com>
2020-06-19 10:01:38 -0500

    working on walker detail

--- Chonker/Controllers/WalkersController.cs
+++ Chonker/Controllers/WalkersController.cs
@@ -13,11 +13,15 @@ namespace Chonker.Controllers
     public class WalkersController : Controller
     {
         private readonly WalkerRepository _walkerRepo;
+        private readonly WalkRepository _walkRepo;
+        private readonly OwnerRepository _ownerRepo;
 
         // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
         public WalkersController(IConfiguration config)
         {
             _walkerRepo = new WalkerRepository(config);
+            _walkRepo = new WalkRepository(config);
+            _ownerRepo = new OwnerRepository(config);
         }
 
         // GET: Walkers
---
+++ Chonker/Models/Walk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chonker.Models
+{
+    public class Walk
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+        public int WalkerId { get; set; }
+        public Walker Walker { get; set; }
+        public int DogId { get; set; }
+        public Dog Dog { get; set; }
+        public int OwnerId { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
